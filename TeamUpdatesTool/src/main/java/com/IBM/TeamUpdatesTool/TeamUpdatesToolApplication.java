2d37d73f14994d318b926ca07e0a8e605b16fbdb
subhashinijayampu <90376149+subhashinijayampu@users.noreply.github.com>
2021-09-09 13:23:07 +0530

    Add files via upload

---
+++ TeamUpdatesTool/src/main/java/com/IBM/TeamUpdatesTool/TeamUpdatesToolApplication.java
@@ -0,0 +1,13 @@
+package com.IBM.TeamUpdatesTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamUpdatesToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamUpdatesToolApplication.class, args);
+	}
+
+}
---
+++ TeamUpdatesTool/src/test/java/com/IBM/TeamUpdatesTool/TeamUpdatesToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IBM.TeamUpdatesTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamUpdatesToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
